857210d797a2a0ebc238746c51f74e4ec008e9ab
Maria <salyginamaria156@gmail.com>
2023-01-15 21:05:20 +0300

    add:  'Вперёд' button works, selecting an item

--- js/main.js
+++ js/main.js
@@ -7,8 +7,12 @@ const blockMain = document.querySelector(".main")
 
 const btnEnter = document.querySelector(".btn-nickname")
 const btnExit = document.querySelector(".btn-exit")
+const btnStart = document.querySelector(".btn-start")
 
 const listItems = document.querySelector(".list-items")
+
+const scoreBot = document.querySelector(".main__score-bot")
+const scoreYou = document.querySelector(".main__score-you")
 
 arrItem.forEach((item)=>{
     let opt = document.createElement("option")
@@ -36,4 +40,44 @@ btnExit.addEventListener("click", ()=>{
     blockMain.style.display = "none"
     let title = document.querySelector(".title")
     title.style.background = "inherit"
+    document.querySelector(".main__item-bot").textContent = '?'
+    document.querySelector(".item-you").textContent = '?'
 })
+
+
+function choicesBot(arr) {
+    let i = Math.round(Math.random()*(arr.length - 1))
+    return arr[i]
+}
+
+function choicesYou() {
+    let item = listItems.value
+    return item
+}
+
+btnStart.addEventListener("click", ()=>{
+    let bot = choicesBot(arrItem)
+    document.querySelector(".main__item-bot").textContent = bot
+    let you = choicesYou()
+    document.querySelector(".item-you").textContent = you
+    check(bot, you)
+    if(parseInt(scoreBot.textContent) == 3 || parseInt(scoreYou.textContent) == 3) {
+        btnStart.style.display = "none"
+    }
+})
+
+function check(itemBot, itemYou) {
+    
+    if((itemBot == 'ножницы'&& itemYou == 'бумага') || (itemBot == 'камень'&& itemYou == 'ножницы') 
+    || (itemBot == 'бумага'&& itemYou == 'камень') ) {
+        scoreBot.textContent = parseInt(scoreBot.textContent) + 1
+    }else if((itemBot == 'ножницы'&& itemYou == 'ножницы') || (itemBot == 'камень'&& itemYou == 'камень') 
+    || (itemBot == 'бумага'&& itemYou == 'бумага')) {
+        scoreBot.textContent = parseInt(scoreBot.textContent) + 1
+        scoreYou.textContent = parseInt(scoreYou.textContent) + 1
+    }else {
+        scoreYou.textContent = parseInt(scoreYou.textContent) + 1
+    }
+        
+    
+}
